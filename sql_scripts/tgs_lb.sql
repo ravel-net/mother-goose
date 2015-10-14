b6881c4f7d814f37ac9754a706eb200192c7f3be
xmei9 <mxysnow@hotmail.com>
2015-10-14 12:42:00 -0700

    load balancer trigger

---
+++ sql_scripts/tgs_lb.sql
@@ -0,0 +1,60 @@
+DROP TABLE IF EXISTS lb_m CASCADE;
+DROP TRIGGER IF EXISTS utm_1 ON utm;
+DROP TRIGGER IF EXISTS lb_tb_1 ON lb_tb;
+
+CREATE TABLE lb_m AS 
+SELECT sid, count(*) AS load
+FROM lb_tb, utm
+WHERE lb_tb.sid = utm.host2
+GROUP BY sid;
+
+CREATE OR REPLACE FUNCTION r_1()
+RETURNS TRIGGER AS
+$$
+if(TD["event"] == "INSERT"):
+	new_host2 = TD["new"]["host2"]
+	m_row = plpy.execute("SELECT * FROM lb_m WHERE sid ="+ str(new_host2))
+	if(m_row.nrows() ==0):
+		plpy.execute("INSERT INTO lb_m VALUES ("+ str(new_host2)+ ", 1);")
+	else:
+		plpy.execute("UPDATE lb_m SET load = load + 1 WHERE sid = "+ str(new_host2) + ";")
+
+if(TD["event"] == "DELETE"):
+	old_host2 = TD["old"]["host2"]
+	plpy.execute("UPDATE lb_m SET load = load - 1 WHERE sid = "+ str(old_host2) + ";")
+	plpy.execute("DELETE FROM lb_m WHERE load = 0;")
+
+return None;
+$$
+LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
+
+
+
+CREATE TRIGGER utm_1
+	BEFORE INSERT OR DELETE ON utm
+	FOR EACH ROW
+EXECUTE PROCEDURE r_1();
+
+
+CREATE OR REPLACE FUNCTION r_2()
+RETURNS TRIGGER AS
+$$
+if(TD["event"] == "DELETE"):
+	old_sid = TD["old"]["sid"]
+	plpy.execute("DELETE FROM lb_m WHERE sid ="+ str(old_sid)+ ";")
+if(TD["event"] == "INSERT"):
+	new_sid = TD["new"]["sid"]
+	res = plpy.execute("SELECT * FROM utm WHERE host2 = "+ str(new_sid)+ ";")
+	count = res.nrows()
+	if not(count ==0):
+		plpy.execute("INSERT INTO lb_m VALUES(" + str(new_sid)+ ", " + str(count)+ ");")
+$$
+LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
+
+CREATE TRIGGER lb_tb_1
+	BEFORE INSERT OR DELETE ON lb_tb
+	FOR EACH ROW
+EXECUTE PROCEDURE r_2();
+
+
+
