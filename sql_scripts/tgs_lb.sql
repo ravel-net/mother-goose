2f30fc09bd5f26e5b799a5628470b99f40c27d2f
xmei9 <mxysnow@hotmail.com>
2015-10-27 18:29:27 -0700

    load balancer

--- sql_scripts/tgs_lb.sql
+++ sql_scripts/tgs_lb.sql
@@ -2,27 +2,37 @@ DROP TABLE IF EXISTS lb_m CASCADE;
 DROP TRIGGER IF EXISTS utm_1 ON utm;
 DROP TRIGGER IF EXISTS lb_tb_1 ON lb_tb;
 
-CREATE TABLE lb_m AS 
-SELECT sid, count(*) AS load
-FROM lb_tb, utm
-WHERE lb_tb.sid = utm.host2
-GROUP BY sid;
+---CREATE TABLE lb_m AS 
+---SELECT sid, count(*) AS load
+---FROM lb_tb, utm
+---WHERE lb_tb.sid = utm.host2
+---GROUP BY sid;
+
+CREATE TABLE lb_m AS(
+	SELECT sid,
+		(SELECT count(*) FROM utm
+		 WHERE host2 = sid) AS load
+	FROM lb_tb
+	);
+
 
 CREATE OR REPLACE FUNCTION r_1()
 RETURNS TRIGGER AS
 $$
 if(TD["event"] == "INSERT"):
 	new_host2 = TD["new"]["host2"]
-	m_row = plpy.execute("SELECT * FROM lb_m WHERE sid ="+ str(new_host2))
-	if(m_row.nrows() ==0):
-		plpy.execute("INSERT INTO lb_m VALUES ("+ str(new_host2)+ ", 1);")
-	else:
+	m_row = plpy.execute("SELECT * FROM lb_tb WHERE sid ="+ str(new_host2))
+	#if(m_row.nrows() ==0):
+		#plpy.execute("INSERT INTO lb_m VALUES ("+ str(new_host2)+ ", 1);")
+	#else:
+	if(m_row.nrows() != 0):
 		plpy.execute("UPDATE lb_m SET load = load + 1 WHERE sid = "+ str(new_host2) + ";")
 
 if(TD["event"] == "DELETE"):
 	old_host2 = TD["old"]["host2"]
 	plpy.execute("UPDATE lb_m SET load = load - 1 WHERE sid = "+ str(old_host2) + ";")
-	plpy.execute("DELETE FROM lb_m WHERE load = 0;")
+	#plpy.execute("delete from lb_m where load = 0;")
+	
 
 return None;
 $$
@@ -42,12 +52,12 @@ $$
 if(TD["event"] == "DELETE"):
 	old_sid = TD["old"]["sid"]
 	plpy.execute("DELETE FROM lb_m WHERE sid ="+ str(old_sid)+ ";")
+
 if(TD["event"] == "INSERT"):
 	new_sid = TD["new"]["sid"]
 	res = plpy.execute("SELECT * FROM utm WHERE host2 = "+ str(new_sid)+ ";")
 	count = res.nrows()
-	if not(count ==0):
-		plpy.execute("INSERT INTO lb_m VALUES(" + str(new_sid)+ ", " + str(count)+ ");")
+	plpy.execute("INSERT INTO lb_m VALUES(" + str(new_sid)+ ", " + str(count)+ ");")
 $$
 LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
 
